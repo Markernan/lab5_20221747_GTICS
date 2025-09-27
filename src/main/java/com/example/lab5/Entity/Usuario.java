28c1c40464abffb20a5d411fb9979b509c311196
Markernan <a20221747@pucp.edu.pe>
2025-09-26 20:10:34 -0500

    ft: usuario entity arreglado/probando push

--- src/main/java/com/example/lab5/Entity/Usuario.java
+++ src/main/java/com/example/lab5/Entity/Usuario.java
@@ -1,6 +1,10 @@
 package com.example.lab5.Entity;
 
 import com.fasterxml.jackson.annotation.JsonTypeId;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,12 +19,46 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
-    @Column(nullable = false)
+    @Column(nullable = false, length =  50)
+
+    @NotBlank(message = "El nombre es obligatorio")
+
+    @Pattern(regexp = "^[a-zA-ZáéíóúÁÉÍÓÚñÑ\\s]+$", message = "El nombre no puede contener números ni caracteres especiales")
+
+
     private String nombre;
-    @Column(nullable = false)
+
+    @Column(nullable = false, length = 50)
+
+    @NotBlank(message = "El apellido es obligatorio")
+
+
     private String apellido;
+
+    @Column(nullable = false, unique = true, length = 100)
+
+    @NotBlank(message = "El correo es obligatorio")
+
+    //va a permitir limitar el formato del correo
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Formato de correo inválido")
+
     private String correo;
+
+    @Column(nullable = false)
+
+    @Min(value = 18, message = "La edad mínima es 18 años")
+
     private int edad;
+
+    @Column(length = 255)
+    @Size(min = 10, message = "La descripción debe tener al menos 10 caracteres")
+
     private String descripcion;
+
+    @Column(length = 255)
+    //va a permitir limitar el formato de la contraseña
+    @Pattern(regexp = "^(?=.*\\d).{6,}$", message = "La contraseña debe tener al menos 6 caracteres y 1 número")
+
     private String contrasena;
+
 }
